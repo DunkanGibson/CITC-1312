37dce78b5e1df3458ce349671bce0f36ea8f7dcb
ChocolateSocrotes <52463084+DunkanGibson@users.noreply.github.com>
2022-01-27 19:31:49 -0500

    Initial commit for 1312
    Initial commit for 1312. Hello World!

---
+++ HelloWorldProgram/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloWorldProgram
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ReadKey();
+        }
+    }
+}
