27a5a83b9f3447e9e645154deda14dd253dc770e
ChocolateSocrotes <52463084+DunkanGibson@users.noreply.github.com>
2022-01-28 15:00:04 -0500

    Assignment 2
    Assignment 2 to create a simple temp converter from Celsius to Fahrenheit

---
+++ TemperatureConversion/TemperatureConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace TemperatureConversion
+{
+    class TemperatureConversion
+    {
+        static void Main(string[] args)
+        {
+            string tempInCelsius;
+
+
+            Console.Write("Please enter a temperature to convert: ");
+
+            tempInCelsius = Console.ReadLine();
+
+            Console.WriteLine("Celsius {0:####.0}" , int.Parse(tempInCelsius));
+            Console.WriteLine("Fahrenheit: {0:F1}" , (float.Parse(tempInCelsius) * 9/5 + 32));
+
+        }
+    }
+}
