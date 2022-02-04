a38ccc1386fd52dd5f0662b8320dd09e4ca494c1
ChocolateSocrotes <52463084+DunkanGibson@users.noreply.github.com>
2022-02-04 18:01:17 -0500

    Full commit of Assignment 3
    Simple console app to grab width and height with methods, calculate area, perimeter, and display results

---
+++ Calculate Dimensions/CalculateDimensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Calculate_Dimensions
+{
+    class CalculateDimensions
+    {
+        static float height;
+        static float width;
+        static void Main(string[] args)
+        {
+            SetHeight();
+            SetWidth();
+            CalculatePerimeterOfRectangle();
+            CalculateAreaOfRectangle();
+            DisplayResults();
+        }
+
+        static void SetHeight()
+        {
+            Console.Write("Enter the Height: ");
+            height = float.Parse(Console.ReadLine());
+        }
+
+        static void SetWidth()
+        {
+            Console.Write("Enter the Width: ");
+            width = float.Parse(Console.ReadLine());
+        }
+
+        static float CalculateAreaOfRectangle()
+        {
+            return width * height;
+        }
+
+        static float CalculatePerimeterOfRectangle()
+        {
+            return 2 * (width * height);
+        }
+
+        static void DisplayResults()
+        {
+
+            Console.WriteLine("Perimeter of the Rectangle is: {0:F1}", CalculatePerimeterOfRectangle());
+            Console.WriteLine("Area of the Rectangle is: {0:F1}", CalculateAreaOfRectangle());
+        }
+    }
+}
