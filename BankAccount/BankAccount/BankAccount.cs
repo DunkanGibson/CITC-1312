c8f18e8f5ad4d2756f3b866241cb51bfa9a6e570
Dunkan Gibson <DunkanGibson@gmail.com>
2022-03-27 11:35:48 -0400

    Added Chapter 10 assignment of a bank account using polymorphism

---
+++ BankAccount/BankAccount/BankAccount.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BankAccount
+{
+    public abstract class BankAccount
+    {
+
+
+        private string bankAccountNickName;
+        private int accountNumber;
+        private int routingNumber;
+        private string denomination;
+        private double balance;
+
+        public double Balance { get => balance; set => balance = value; }
+        public string Denomination { get => denomination; set => denomination = value; }
+        public int RoutingNumber { get => routingNumber; set => routingNumber = value; }
+        public int AccountNumber { get => accountNumber; set => accountNumber = value; }
+        public string BankAccountNickName { get => bankAccountNickName; set => bankAccountNickName = value; }
+
+        public BankAccount()
+        {
+
+        }
+
+        public BankAccount(string BankAccountNickName, int AccountNumber, int RoutingNumber, string Denomination, double Balance)
+        {
+            bankAccountNickName = BankAccountNickName;
+            accountNumber = AccountNumber;
+            routingNumber = RoutingNumber;
+            denomination = Denomination;
+            balance = Balance;
+        }
+
+
+
+        public abstract double Withdraw(double withdrawl);
+
+        public abstract double Deposit(double deposit);
+
+
+
+
+
+    }
+}
---
+++ BankAccount/BankAccount/CheckingAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace BankAccount
+{
+    public class CheckingAccount : BankAccount
+    {
+        public CheckingAccount()
+        {
+        }
+
+
+        public CheckingAccount(string bankAccountNickName, int accountNumber, int routingNumber, string denomination, double balance)
+        {
+            BankAccountNickName = bankAccountNickName;
+            AccountNumber = accountNumber;
+            RoutingNumber = routingNumber;
+            Denomination = denomination;
+            Balance = balance;
+        }
+
+        public override double Withdraw(double withdrawlAmmount)
+        {
+            return Balance - withdrawlAmmount;
+        }
+
+        public override double Deposit(double depositAmmount)
+        {
+            return Balance + depositAmmount;
+        }
+
+    }
+}
